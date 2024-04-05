cffce7ffbf00f906702e5438b61a0fe6728a09c9
Tyler O'Neil <tyleroneildev@gmail.com>
2024-04-04 20:25:44 -0400

    Format Player

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,7 @@ public class Player {
     public String name;
     public Colour colour;
 
-    public Player(String name, Colour colour){
+    public Player(String name, Colour colour) {
         this.name = name;
         this.colour = colour;
     }
